37aec07fc7783778bad4dd9e675209fe728324d4
Taha Imtiaz <taha@inspakt.com>
2024-06-27 15:49:45 +0300

    ViewModel added, along with UI design

--- app/src/main/java/com/example/jettrivia/MainActivity.kt
+++ app/src/main/java/com/example/jettrivia/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.example.jettrivia
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.jettrivia.screen.QuestionsViewModel
+import com.example.jettrivia.screen.TriviaHome
 import com.example.jettrivia.ui.theme.JetTriviaTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -30,26 +26,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Composable
-fun TriviaHome(viewModel: QuestionsViewModel = hiltViewModel()) {
-    Questions(viewModel)
-}
-
-@Composable
-fun Questions(viewModel: QuestionsViewModel) {
-    val questions = viewModel.data.value.data?.toMutableList()
-    if (viewModel.data.value.loading == true) {
-        Log.e("Loading", "Questions: Loading...")
-    } else {
-        questions?.forEach { questionItem ->
-            Log.e("Loading", "Questions: Loading Stopped...")
-            Log.e("Result", "Questions: ${questionItem.question}")
-        }
-    }
-
-    Log.e("SIZE", "Questions: ${questions?.size}")
 }
 
 @Preview(showBackground = true)
